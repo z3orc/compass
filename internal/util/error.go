326faa3d12649ce5a38848c83933718c5cd2cd71
z3orc <z3orc@users.noreply.github.com>
2022-12-28 00:25:22 +0100

    Change response messages

--- internal/util/error.go
+++ internal/util/error.go
@@ -4,10 +4,11 @@ import "net/http"
 
 func Error(writer http.ResponseWriter, err error) {
 	if err.Error() == "404" {
-		http.Error(writer, "Could not find version", http.StatusNotFound)
+		http.Error(writer, "404 Not Found -- Could not requested version", http.StatusNotFound)
 	} else if err.Error() == "503" {
-		http.Error(writer, "Service Unavailable", http.StatusServiceUnavailable)
+		http.Error(writer, "503 Service Unavailable -- Could not reach external resources", http.StatusServiceUnavailable)
 	} else {
-		http.Error(writer, "Bad Request", http.StatusBadRequest)
+		http.Error(writer, "400 Bad Request -- Could not process request", http.StatusBadRequest)
 	}
+	
 }
