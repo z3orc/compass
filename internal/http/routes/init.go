c63d1d3b628a1ed4ac63556d4045aa5de90ac59f
z3orc <z3orc@users.noreply.github.com>
2022-12-28 19:35:52 +0100

    Notin

--- internal/http/routes/init.go
+++ internal/http/routes/init.go
@@ -5,6 +5,24 @@ import (
 	"github.com/z3orc/dynamic-rpc/internal/http/handler"
 )
 
+// func Init(router *chi.Mux) {
+// 	router.Route("/vanilla", func(r chi.Router){
+// 		r.Get("/{id}", handler.VanillaAsJson)
+// 		r.Get("/{id}/download", handler.VanillaAsRedirect)
+// 	})
+// 	// s := router.PathPrefix("/vanilla").Subrouter()
+// 	// s.HandleFunc("/{id}", handler.VanillaAsJson)
+// 	// s.HandleFunc("/{id}/download", handler.VanillaAsRedirect)
+
+// 	// s = router.PathPrefix("/paper").Subrouter()
+// 	// s.HandleFunc("/{id}", handler.PaperAsJson)
+// 	// s.HandleFunc("/{id}/download", handler.PaperAsRedirect)
+
+// 	// s = router.PathPrefix("/purpur").Subrouter()
+// 	// s.HandleFunc("/{id}", handler.PurpurAsJson)
+// 	// s.HandleFunc("/{id}/download", handler.PurpurAsRedirect)
+// }
+
 func Init(router *mux.Router) {
 	s := router.PathPrefix("/vanilla").Subrouter()
 	s.HandleFunc("/{id}", handler.VanillaAsJson)
