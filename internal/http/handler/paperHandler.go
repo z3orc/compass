c7abc243b458a37a236c2147159011357cd29a11
z3orc <z3orc@users.noreply.github.com>
2022-12-28 23:14:00 +0100

    Change param

--- internal/http/handler/paperHandler.go
+++ internal/http/handler/paperHandler.go
@@ -3,13 +3,13 @@ package handler
 import (
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi/v5"
 	"github.com/z3orc/dynamic-rpc/internal/client/paper"
 	"github.com/z3orc/dynamic-rpc/internal/util"
 )
 
 func PaperAsJson(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["id"]
+	id := chi.URLParam(r, "id")
 
     version, err := paper.GetFormatted(id)
     if err != nil {
@@ -21,7 +21,7 @@ func PaperAsJson(w http.ResponseWriter, r *http.Request) {
 }
 
 func PaperAsRedirect(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["id"]
+	id := chi.URLParam(r, "id")
 
     url, err := paper.GetDownloadUrl(id)
     if err != nil {
