d4794c58f5e83e17c0db89b07105f13123132172
z3orc <z3orc@users.noreply.github.com>
2022-12-28 19:35:44 +0100

    New recorder

---
+++ internal/http/recorder/recorder.go
@@ -0,0 +1,24 @@
+package recorder
+
+import "net/http"
+
+type ResponseRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+	Body []byte
+}
+
+func (rec *ResponseRecorder) WriteHeader(statusCode int) {
+	rec.StatusCode = statusCode
+	rec.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rec *ResponseRecorder) Write(body []byte) (int, error) {
+	rec.Body = body
+	return rec.ResponseWriter.Write(body)
+}
+
+// func (rec *ResponseRecorder) Header(body []byte) (int, error) {
+// 	rec.Header = body
+// 	return rec.ResponseWriter.Write(body)
+// }
