092bf2eb1f6fcb8d22b840363c97071a4e6a5182
z3orc <z3orc@users.noreply.github.com>
2022-12-29 15:57:18 +0100

    Add baseURL

--- internal/http/handler/redirect.go
+++ internal/http/handler/redirect.go
@@ -7,9 +7,13 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+const baseURL = "https://api.compass.z3orc.com"
+
 func Redirect(w http.ResponseWriter, r *http.Request) {
 	flavour := chi.URLParam(r, "flavour")
 	id := chi.URLParam(r, "id")
+    
+    newURL := fmt.Sprintf("%s/%s/%s", baseURL, flavour, id)
 
-	http.Redirect(w, r, fmt.Sprint("https://api.compass.z3orc.com/", flavour, "/", id), http.StatusPermanentRedirect)
+	http.Redirect(w, r, newURL, http.StatusPermanentRedirect)
 }
