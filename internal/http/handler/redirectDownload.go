f2ac23f45f08388ba7d496ba407b1ecdca773b2e
z3orc <z3orc@users.noreply.github.com>
2022-12-30 15:08:11 +0100

    Remove additional "/"

--- internal/http/handler/redirectDownload.go
+++ internal/http/handler/redirectDownload.go
@@ -11,7 +11,7 @@ func RedirectDownload(w http.ResponseWriter, r *http.Request) {
 	flavour := chi.URLParam(r, "flavour")
 	id := chi.URLParam(r, "id")
     
-    newURL := fmt.Sprintf("%s/%s/%s/%s", baseURL, flavour, id, "/download")
+    newURL := fmt.Sprintf("%s/%s/%s/%s", baseURL, flavour, id, "download")
 
 	http.Redirect(w, r, newURL, http.StatusPermanentRedirect)
 }
