9c95e4dd7c585b24c0b70df261e21b3b1bae15cb
z3orc <z3orc@users.noreply.github.com>
2023-05-15 23:31:45 +0200

    new homepage

--- internal/http/handler/home.go
+++ internal/http/handler/home.go
@@ -1,7 +1,16 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/common-nighthawk/go-figure"
+	"github.com/z3orc/dynamic-rpc/internal/env"
+)
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome! Usage: /{flavour}/{version}"))
+	banner := figure.NewColorFigure("COMPASS", "slant", "cyan", true)
+	figure.Write(w, banner)
+	w.Write([]byte("\nWelcome! Compase is here to help you fetch whichever version of the Minecraft Server Jar you would like \nboth from a browser and from the command-line! The tool has been made to work with both vanilla, paper\nand purpur. The only thing that remain is giving it a try! Usage: /{flavour}/{version} \n"))
+	w.Write([]byte(fmt.Sprintf("\n\nThis instance is current running v%s_%s.", env.Version, env.Build)))
 }
