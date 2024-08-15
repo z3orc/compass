a204be90d35389633006c64d88b22e61cb21499b
z3orc <z3orc@users.noreply.github.com>
2024-08-15 23:52:27 +0200

    test: getversion from repo

--- internal/repo/version_test.go
+++ internal/repo/version_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/z3orc/compass/internal/data"
+	"github.com/z3orc/compass/internal/model"
 )
 
 func TestNewVersionRepositoryOneSource(t *testing.T) {
@@ -39,5 +40,48 @@ func TestNewVersionRepositoryNoSource(t *testing.T) {
 
 	if repo != nil {
 		t.Fatalf("Expected a nil pointer but got %T", repo)
+	}
+}
+
+func TestGetVersion(t *testing.T) {
+	src := data.NewPistonDataSource()
+	repo := NewVersionRepository(src)
+	selectedVersion := "1.21"
+
+	version, err := repo.GetVersion(model.FlavourPiston, selectedVersion)
+	if err != nil {
+		t.Fatalf("Expected no error but got %v", err)
+	}
+
+	if version == nil {
+		t.Fatal("Expected *model.Version got nil")
+	}
+
+	if version.Flavour != model.FlavourPiston {
+		t.Fatalf("Expected flavour to be %v but got %v", model.FlavourPiston, version.Flavour)
+	}
+
+	if version.Id != selectedVersion {
+		t.Fatalf("Expected id to be %v but got %v", selectedVersion, version.Id)
+	}
+
+	// if version.Url != "https://piston-meta.mojang.com/v1/packages/1.2112312/1.2112312.json" {
+	// 	t.Fatalf("Expected url to be %v but got %v", "https://piston-meta.mojang.com/v1/packages/1.2112312/1.2112312.json", version.Url)
+	// }
+
+}
+
+func TestGetVersionError(t *testing.T) {
+	src := data.NewPistonDataSource()
+	repo := NewVersionRepository(src)
+	selectedVersion := "1.21"
+
+	version, err := repo.GetVersion(model.FlavourPaper, selectedVersion)
+	if err == nil {
+		t.Fatal("Expected an error but got nil")
+	}
+
+	if version != nil {
+		t.Fatalf("Expected a nil pointer but got %T", version)
 	}
 }
